be03732ba724368d544113464ae3ef560f396927
Cplus Shen <cplus.shen@gmail.com>
2019-11-04 01:59:29 +0000

    added restore-kvm-2 script and fixed unzip error

---
+++ recover/restore-kvm-2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+scp cplus@or:/home/backup/dev-cloud/atca-9113-wr-1_2019-1004.tgz .
+scp cplus@or:/home/backup/dev-cloud/dio-yocto-1_2019-1004.tgz .
+scp cplus@or:/home/backup/dev-cloud/esp-9010b_2019-1004.tgz .
+scp cplus@or:/home/backup/dev-cloud/esp-9400-terryt_2019-1004.tgz .
+scp cplus@or:/home/backup/dev-cloud/kernel_2019-1004.tgz .
