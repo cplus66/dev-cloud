cd81ba7307275a2e0cad6170eaf9a670b02c9be5
Cplus Shen <cplus.shen@gmail.com>
2019-11-05 13:37:05 +0800

    added garfield to hosts

--- mgmt.sh
+++ mgmt.sh
@@ -7,5 +7,7 @@ case "$1" in
   ssh docker-1 "sudo apt-get update"
   ssh docker-2 "sudo apt-get update"
   ;;
+  hosts)
+  ssh kvm-1    'sudo bash -c "echo \"172.17.5.235 ga\" >> /etc/hosts"'
 
 esac
