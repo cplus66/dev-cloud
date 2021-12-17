526c6e27db8f138984e9d2683da5742f9613409e
Cplus Shen <cplus.shen@gmail.com>
2021-12-17 14:54:42 +0800

    added run remote commands from script.

--- mgmt-vm.sh
+++ mgmt-vm.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+#!/bin/bash -e
+
 LIST="atca-9113-wr8-2 \
 bcn-ipv6-0 \
 bcn-ipv6-1 \
@@ -19,15 +20,44 @@ yocto-3 \
 yocto-4 \
 "
 
+function usage()
+{
+  echo "Usage: $0 <cmd> | $0 -f <shell_script>"
+}
+
 if [ x$DEBUG != "x" ]; then
   LIST=yocto-4
 fi
 
 if [ $# -eq 0 ]; then
-  echo "Usage: $0 <cmd>"
+  usage
   exit 0
 fi
 
+
+if [ x$1 == "x-f" ]; then
+  if [ x$2 != "x" ]; then
+    if [ -f $2 ]; then
+      tmpfile=$2
+    else
+      echo "File $2 doesn't exist" 
+    fi
+  else
+    echo "Usage $0 -f <shell_script>"
+    exit 0
+  fi
+else
+  tmpfile=$(mktemp /tmp/mgmt.XXX)
+  REMOTE_CMD=$@
+
+  echo $REMOTE_CMD > $tmpfile 
+fi
+
 for i in $LIST; do
-  ssh $i $@
+  echo -n "$i, "
+  ssh $i /bin/bash -s < $tmpfile
 done
+
+if [ x$REMOTE_CMD != "x" ]; then
+  rm -f $tmpfile
+fi
