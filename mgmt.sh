590708901ca2fa8164368e65b40203215327e456
Cplus Shen <cplus.shen@gmail.com>
2021-12-17 16:09:31 +0800

    renamed mgmt-vm.sh to mgmt.sh

--- mgmt.sh
+++ mgmt.sh
@@ -1,30 +1,22 @@
 #!/bin/bash -e
 
-LIST="atca-9113-wr8-2 \
-bcn-ipv6-0 \
-bcn-ipv6-1 \
+LIST="\
+atca-9113-wr8-2 \
+bcn-ipv6-0 bcn-ipv6-1 \
 dio-yocto-2 \
 esp-3000-ubu14-0 \
 esp-9010-deb10-1 \
 esp-9010b-1 \
-jetpack-0 \
-jetpack-1 \
-jetpack-2 \
-jetpack-3 \
-jetpack-4 \
+jetpack-0 jetpack-1 jetpack-2 jetpack-3 jetpack-4 \
 vega-3500-sc-0 \
-yocto-0 \
-yocto-1 \
-yocto-2 \
-yocto-3 \
-yocto-4 \
+yocto-0 yocto-1 yocto-2 yocto-3 yocto-4
 "
 
 KVM_LIST="kvm-1 kvm-2 kvm-3 docker-1 docker-2"
 
 function usage()
 {
-  echo "Usage: $0 <cmd> | $0 -f <shell_script>"
+  echo "Usage: {DEBUG=1} {KVM=1} $0 <cmd> | $0 -f <shell_script>"
 }
 
 if [ "x$DEBUG" != "x" ]; then
