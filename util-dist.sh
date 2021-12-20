1e82279c0d4f0e51d1e05d89aa195dad7990e406
Cplus Shen <cplus.shen@gmail.com>
2021-12-20 15:10:59 +0800

    added cpu number, memory size and disk information.

--- util-dist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-which lsb_release > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
-  lsb_release -d | awk -F ":" '{print $2}' | awk '{$1=$1;print}'
-else
-  hostnamectl | grep "Operating System" | awk -F ":" '{print $2}'| awk '{$1=$1;print}'
-fi
