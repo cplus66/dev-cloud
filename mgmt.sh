5ce7c3db80cf62a7d3d49f1f928efbfa2e226bb2
Cplus Shen <cplus.shen@gmail.com>
2023-06-14 15:31:28 +0800

    updatd VM host list.

--- mgmt.sh
+++ mgmt.sh
@@ -1,16 +1,6 @@
 #!/bin/bash -e
 
-LIST="\
-atca-9113-wr8-2 \
-bcn-ipv6-0 bcn-ipv6-1 \
-dio-yocto-2 \
-esp-3000-ubu14-0 \
-esp-9010-deb10-1 \
-esp-9010b-1 \
-jetpack-0 jetpack-1 jetpack-2 jetpack-3 jetpack-4 \
-vega-3500-sc-0 \
-yocto-0 yocto-1 yocto-2 yocto-3 yocto-4
-"
+LIST=$(cat host-vm.conf)
 
 KVM_LIST="kvm-1 kvm-2 kvm-3 docker-1 docker-2"
 
