f788ab43e01d3d96777c2c9f6a58731f73d3db06
Cplus Shen <cplus.shen@gmail.com>
2026-03-20 10:49:17 +0800

    added VM search script.

---
+++ virsh-find.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+LIST=$(cat host-server.conf | grep -v or)
+
+function usage()
+{
+  echo "$0 <command>"
+}
+
+if [ $# -eq 0 ]; then
+  usage
+  exit 0
+fi
+
+for i in $LIST; do
+  echo "[$i]"
+  virsh -c qemu+ssh://$i/system domblklist $1 | grep "\S"
+done
